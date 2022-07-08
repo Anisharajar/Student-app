93ce8c1c534aecb327d0b952e3658a5d3e9b5d9d
Anisharajar <anisharajar.2002@gmail.com>
2022-07-08 11:03:47 +0530

    Active

--- src/app/view-student/view-student.component.ts
+++ src/app/view-student/view-student.component.ts
@@ -11,5 +11,14 @@ export class ViewStudentComponent implements OnInit {
 
   ngOnInit(): void {
   }
-name="Ananya"
+data=[
+  {name:"Anu",age:"20"}
+  {name:"Anoop",age:"29"}
+  {name:"Ananya",age:"22"}
+  {name:"Dahya",age:"21"}
+  {name:"Majunu",age:"27"}
+  {name:"Diya",age:"25"}
+  {name:"Laya",age:"25"}
+
+]
 }
